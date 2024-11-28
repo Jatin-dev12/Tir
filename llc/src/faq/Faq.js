d399e0cfdccb4bd37b07b01b0a300a7e7b190b38
jatin sehgal <jatinsehgal124@gmail.com>
2024-11-28 13:17:25 +0530

    lateest

--- llc/src/faq/Faq.js
+++ llc/src/faq/Faq.js
@@ -3,34 +3,51 @@ import {
   AccordionItem,
   AccordionItemContent,
   AccordionItemTrigger,
-  AccordionRoot,
+  AccordionRoot,Box,Heading
 } from "@chakra-ui/react"
 import { LoremIpsum } from "react-lorem-ipsum"
+import { MdKeyboardArrowDown } from "react-icons/md";
+
 
 const items = [
-  { value: "a", title: "First Item", text: <LoremIpsum p={1} /> },
-  { value: "b", title: "Second Item", text: <LoremIpsum p={1} /> },
-  { value: "c", title: "Third Item", text: <LoremIpsum p={1} /> },
+  { value: "a", title: "Are you looking to escalate you business to global market?", text:"We estimate the time needed to complete your project based on the content of your presentation and our experience with similar projects." },
+  { value: "b", title: "What do we provide? ", text: <LoremIpsum p={1} /> },
+  { value: "c", title: "Lorem ipsum dolor sit amet consectetur?", text: <LoremIpsum p={1} /> },
+  { value: "d", title: "Lorem ipsum dolor sit amet consectetur?", text: <LoremIpsum p={1} /> },
+
+  { value: "e", title: "Lorem ipsum dolor sit amet consectetur?", text: <LoremIpsum p={1} /> },
+
 ]
 
 const Faq = () => {
   return (
     <section className="Faq">
+        <Box className="f-box" pb={5}>
+        <Heading padding={10} colorPalette="white">
+        Why Trust Us?
+        </Heading>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Id mattis placerat congue nam
+          <br />
+          Enim nunc vulputate in nunc risus aliquet mattis quisque.
+        </p>
+      </Box>
+      <Box className="accordins">
     <AccordionRoot collapsible>
       {items.map((item, index) => (
         <AccordionItem key={index} value={item.value}>
           <AccordionItemTrigger>
             <Stack gap="1">
-              <Text>{item.title}</Text>
-              <Text fontSize="sm" color="fg.muted">
-                Click to expand
-              </Text>
+              <Text className="text-active">{item.title}</Text>
+              <span className="accordion-arrow"><MdKeyboardArrowDown />
+              </span>
             </Stack>
           </AccordionItemTrigger>
           <AccordionItemContent>{item.text}</AccordionItemContent>
         </AccordionItem>
       ))}
     </AccordionRoot>
+    </Box>
     </section>
   )
 }
